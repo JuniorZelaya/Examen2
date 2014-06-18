8c533b8da36d14057f3c1ccc9a25ca8a28301767
JuniorZelaya <JuniorZelaya26@gmail.com>
2014-06-18 07:52:06 -0600

    Update Ejercicios.java

--- src/Ejercicios.java
+++ src/Ejercicios.java
@@ -5,20 +5,26 @@ public class Ejercicios
 	//2 puntos
 	static boolean esMultiploDeTres(int num)
 	{
-		if ( (num % 3)== 0) // se evalua la condicion,
+		if ( (num % 3)== 0) 
 		{
-			return true; // devuelve tru si es verdadero
-        
+			return true; 
+       
 		}
-		return false; // devuelve false si no cumple la funcion
-		
-		}
+		return false;
+	}
+		//devuelve el numero menor entre a, b o c
+	//Primero creamos la variable resultado para almacenar el resultado
+
 	
 	
 	//devuelve el numero menor entre a, b o c
 	//3 puntos
 	static int getMenor(int a, int b, int c)
 	{
+			int resultado=0;
+	//veremos si las condiciones de la variables e menor
+		//comparando el valor entre ellas.
+
 		int resultado = 0;
 		{
 			if ( a<b && a<c)
@@ -34,23 +40,34 @@ public class Ejercicios
 				return c;
 			}
 		}
+		//Para cada evaluacion activamos la funcion y la almacenamos en nuestra variable
+	//resultado listo para mostrar en pantalla
 		return resultado;
 		
 			}
 	
+	//devolvera los elementos del arreglo
+
 	
 	
 	//devuelve el promedio los elementos de un arreglo
 	//5 puntos
 	static int getPromedio(int arreglo[])
 	{
+		//Primero creamos una variable llamada resultado para almacenar los calculos
+		//crearemos una variable llamada resultado para guardar calculos//
 		int resultado = 0;
 		int cantidad_numeros = 0;
+		//Usamos FOR para que recorra el arreglo y acumula dentro de la variable(cantidad_numeros) el 
+
 		for(int i = 0; i< arreglo.length; i++)
 		{
 			resultado += arreglo [i];
 			cantidad_numeros ++;
 		}
+		// al terminar todo el arreglo... 
+	//dividira la suma de todos 
+	//elementos que tiene el arreglo. 
 		resultado = (resultado/cantidad_numeros);
 		return resultado ;
 		}
@@ -80,15 +97,15 @@ public class Ejercicios
 	//1 punto oro extra
 	static int funcionRecursiva(int num)
 	{
-		int resultado;
-		if (num == 0)
+		int resultado;//se hace una variable
+		if (num == 0)//se evalua la condicion
 		{
 			resultado = 0;
-		}else
+		}else //se establece el else del if para terminar de evaluar
 		{
 			resultado = num + funcionRecursiva(num-1);
 	}
-	return resultado;
+	return resultado; //devuelve el resultado
 }
 
 		
